443c89c11eeeb3365d974ced0d62e41c9c778602
krychkarol <krych.karol.93@gmail.com>
2022-07-18 16:55:07 +0200

    create player

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 
-canvas.style.width = '100%';
-canvas.style.height = '100%';
+canvas.width = innerWidth;
+canvas.height = innerHeight;
+
+const playerX = canvas.width / 2;
+const playerY = canvas.height / 2;
+
+class Player {
+    constructor(x, y, size, color) {
+        this.x = x;
+        this.y = y;
+        this.size = size;
+        this.color = color;
+    }
+
+    draw() {
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2, false);
+        ctx.fillStyle = this.color;
+        ctx.fill();
+    }
+}
+
+const player = new Player(playerX, playerY, 20, 'red');
+player.draw();
