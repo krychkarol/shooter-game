81d9d2d87e3190061ed242319da5e0ea0c6e9c95
krychkarol <krych.karol.93@gmail.com>
2022-07-25 18:09:48 +0200

    shooting bullets

--- index.js
+++ index.js
@@ -23,5 +23,50 @@ class Player {
     }
 }
 
-const player = new Player(playerX, playerY, 20, 'red');
-player.draw();
+class Bullet {
+    constructor(x, y, size, color, velocity) {
+        this.x = x;
+        this.y = y;
+        this.size = size;
+        this.color = color;
+        this.velocity = velocity;
+    }
+
+    draw() {
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2, false);
+        ctx.fillStyle = this.color;
+        ctx.fill();
+    }
+
+    update() {
+        this.draw();
+        this.x = this.x + this.velocity.x;
+        this.y = this.y + this.velocity.y;
+    }
+}
+
+const player = new Player(playerX, playerY, 30, 'red');
+
+const bullets = [];
+const bullet = new Bullet(playerX, playerY, 10, 'yellow', {x: 1, y: 1});
+
+function animation(){
+    requestAnimationFrame(animation);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    player.draw();
+    bullets.forEach(bullet => {
+        bullet.update()
+    })
+};
+
+addEventListener('click', (e) => {
+    const angle = Math.atan2(e.clientY - playerY, e.clientX - playerX);
+    const velocity = {
+        x: Math.cos(angle),
+        y: Math.sin(angle)
+    }
+    bullets.push(new Bullet(playerX, playerY, 10, 'yellow', velocity))
+});
+
+animation();
